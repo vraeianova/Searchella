2329a3d7bac5ff2a366189c2e0cb5293ba3669c4
Cris Arias <vraeianova@gmail.com>
2023-01-05 17:13:04 -0600

    add: production server

--- main/server.go
+++ main/server.go
@@ -1,27 +1,66 @@
 package main
 
 import (
+	"io"
+	"log"
 	"net/http"
-	"time"
+	"strings"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
-type SearchellaServer struct {
-	server *http.Server
-}
+func main() {
+	r := chi.NewRouter()
 
-func NewServer(mux *chi.Mux) *http.Server {
-	s := &http.Server{
-		Addr:           ":9000",
-		Handler:        mux,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-	return s
-}
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
-func (s *SearchellaServer) Run() {
-	s.server.ListenAndServe()
+	r.Get("/api/search/{search_phrase}/", func(w http.ResponseWriter, r *http.Request) {
+
+		search_phrase := chi.URLParam(r, "search_phrase")
+
+		query := `{
+			"search_type": "match",
+			"query":
+			{
+				"term": "` + search_phrase + `"	
+			},
+			"from": 0,
+			"max_results": 20,
+			"_source": []
+		}`
+
+		req, err := http.NewRequest("POST", "http://localhost:4080/api/messages/_search", strings.NewReader(query))
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.SetBasicAuth("admin", "Complexpass#123")
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		log.Println(resp.StatusCode)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write([]byte(body))
+	})
+
+	http.ListenAndServe(":8080", r)
 }
